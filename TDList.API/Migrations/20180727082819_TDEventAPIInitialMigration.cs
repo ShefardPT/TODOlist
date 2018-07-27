3280191d3e6cea8ef3ff2ddb092865cfe8fbcd6e
ShefardAA <shefard55r@yandex.ru>
2018-07-27 15:06:00 +0600

    Migration added

---
+++ TDList.API/Migrations/20180727082819_TDEventAPIInitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TDList.API.Migrations
+{
+    public partial class TDEventAPIInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TDEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 240, nullable: true),
+                    Importance = table.Column<int>(nullable: false),
+                    Urgency = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TDEvents", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TDEvents");
+        }
+    }
+}
