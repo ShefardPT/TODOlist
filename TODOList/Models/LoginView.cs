3618fe8ce9601224bb5bc910c26859f6055f9894
ShefardAA <shefard55r@yandex.ru>
2018-08-23 11:20:25 +0600

    Login is now available

---
+++ TODOList/Models/LoginView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODOList.Models
+{
+    public class LoginView
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Remember password")]
+        public bool RememberPassword;
+
+        public string ReturnUrl { get; set; }
+    }
+}
